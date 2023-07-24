011b3e6791b3f59bb79923341761dfd125fddc89
shubham mehta <shubhammehta2003@gmail.com>
2023-07-24 13:58:14 +0530

    bava

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import head from '../images/head.png'
-import './Home.css'
-import image from '../images/7string.png'
+import React from "react";
+import head from "../images/head.png";
+import "./Home.css";
+import image from "../images/7string.png";
+import Carousel from "react-bootstrap/Carousel";
+import { dividerClasses } from "@mui/material";
+
+import * as ReactDOM from "react-dom";
 
 const Home = () => {
-    return (
-        <div >
+  return (
+    <div>
+      <Carousel style={{marginTop:'100px'}} >
+        <Carousel.Item interval={1000}>
+        <div style={{backgroundImage: "url($(./images/7string.png))"}} >
+        {/* <div style={{backgroundImage: `url(${image})`}}> */}
 
-            <div className='main' stlye={{ backgroundColor: "" , paddingLeft:'350px'}}>
-                <div className='left'>
-                    <img className='left-img' src={image} alt="movie" />
-                </div>
-                <div className='right'>
-                    <div className='right-img'> 
-                        <img src={head}></img>
-                    </div>
-                    <div >
-                        <p className='right-lower'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>  Aliquam vero, inventore facilis laborum sapiente <br></br> dignissimos magnam quas. Dolor iure possimus fugiat, vel ratione porro <br></br> ipsum expedita officia doloremque. Aliquid, consequuntur!</p>
-                    </div>
-                </div>
-            </div>
 
-        </div>
-    )
-}
 
-export default Home
+          <Carousel.Caption>
+            <h3 style={{color:'white'}}>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel> 
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 export default function Landing() {
   return (
     <>
-        <div style={{backgroundColor: "#2b2c2c",position: "fixed",top: 0,left: 0,width: "100%",height: "100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <div style={{backgroundColor: "white",position: "fixed",top: 0,left: 0,width: "100%",height: "100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
         <motion.div style={{ width: "45%", margin: "auto",marginBottom:"100px" }} initial={{opacity:0}} animate={{opacity:1.5}} transition={{duration:5,delay:0.8}}>
         <img src={hanuman2} alt="Hanuman" style={{ width: "80%", paddingLeft:'130px' }} />
         </motion.div>
