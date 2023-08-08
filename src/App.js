32679f728c54824c942efac4b19a9f177e814c9c
Vatsal Shah <shahvatsal2003@gmail.com>
2023-08-08 16:43:55 +0530

    hehe

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Landing from './components/Landing';
-import Contact from './components/Contact';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AboutUs from './components/AboutUs';
-import Home from './components/Home';
-import Project from './components/Project';
-import SlaveDetails from './components/SlaveDetails';
-import Movies from './components/Movies';
-import Land from './components/Land';
-
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
+import Home from "./components/Home";
+import Project from "./components/Project";
+import SlaveDetails from "./components/SlaveDetails";
+import Movies from "./components/Movies";
+import Land from "./components/Land";
 
 const App = () => {
-  
-
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Land/>}></Route>
-        </Routes>     
-         <Routes>
-           <Route path="/movie" element={<><Movies></Movies></>}></Route>
-         </Routes>
-      
-       </>
+      <Routes>
+        <Route path="/home" element={<Navbar/>}></Route>
+
+        <Route path="/" element={<Landing />}></Route>
+
+        <Route
+          path="/movie"
+          element={
+            <>
+              <Movies></Movies>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </>
   );
-}
+};
 export default App;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,21 @@ import hanuman2 from '../images/jaihaunuman.png'
 // import hanuman2 from '../images/hanuman2.png'
 import { motion } from 'framer-motion'
 import './Navbar.css'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Landing() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate("/home"); // Navigate to the /home route (Navbar)
+    }, 4000);
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
   return (
     <>
         <div className='fullbackground1' style={{position: "fixed",top: 0,left: 0,width: "100%",height: "100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
